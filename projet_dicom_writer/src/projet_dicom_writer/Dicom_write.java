655d76e7467ef7ba9a7c3be6df819902a6341311
Vincentarno <74413292+Vincentarno@users.noreply.github.com>
2020-11-16 10:25:59 +0100

    dicom write

--- projet_dicom_writer/src/projet_dicom_writer/Dicom_write.java
+++ projet_dicom_writer/src/projet_dicom_writer/Dicom_write.java
@@ -1,5 +1,42 @@
 package projet_dicom_writer;
+import java.io.*;
+import java.io.ByteArrayOutputStream;
+
+
 
 public class Dicom_write {
+	
+
+
+	//package dicom_writer;
+
+
+		String nom_fichier;
+		
+		
+		public void set_nom_fichier(String nom) {
+			this.nom_fichier=nom;
+			
+		}
+		public void set_data_VRstrict(int tag, String VR, int longueur, int value) {
+			/*Nombre est le TAG
+			 * Utilisation du code Endian Data Input Stream modifié.... */
+			
+			
+			
+			
+		}
+		public void	set_data_VR(int nombre, String VR, int longueur, int value) {
+		
+			
+			
+			
+			
+		}
+		/*public void open (String NomDuFichier) {
+			Salut des soucisses
+		}*/
+		
 
 }
+
