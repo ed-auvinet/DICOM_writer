2f5259f492a068694254c211b946d04fe5a39681
antoninhalut <74411863+antoninhalut@users.noreply.github.com>
2020-11-23 20:25:09 +0100

    Intégration écriture Dicom, mise en forme en int[], préparation pour outpitstream

--- projet_dicom_writer/src/projet_dicom_writer/Dicom_write.java
+++ projet_dicom_writer/src/projet_dicom_writer/Dicom_write.java
@@ -1,41 +1,97 @@
 package projet_dicom_writer;
 import java.io.*;
 import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
 
 
 
 public class Dicom_write {
+	private String nom_fichier;
+	private ByteArrayOutputStream Sortie;
+	private byte octetvalue;
+	private ArrayList<Integer> list_tempo; //utiliser list<Integer> instead ?
 	
-
-
 	//package dicom_writer;
-
-
-		String nom_fichier;
-		
-		
-		public void set_nom_fichier(String nom) {
+	public Dicom_write(int total_size) {
+		/*Prepare le dicom avec le preambule et la norme*/
+		int i;
+		for (i=0; i<127;i++) { 
+				list_tempo.add(0x00);	//plus sûr on aurait pu utilisé 0 aussi peut-être
+		}
+		list_tempo.add(0x44);
+		list_tempo.add(0x49);
+		list_tempo.add(0x43);
+		list_tempo.add(0x4D);
+	}
+	
+	
+	
+	public void set_nom_fichier(String nom) {
 			this.nom_fichier=nom;
 			
 		}
-		public void set_data_VRstrict(int tag, String VR, int longueur, int value) {
+		public void set_data_VRstrict(String tag, String VR, int longueur, int[] value) {
 			/*Nombre est le TAG
-			 * Utilisation du code Endian Data Input Stream modifié.... */
+			 * Utilisation du code Endian Data Input Stream modifié.... à ajouter ici à chaque étapes */
+			char VR1=VR.charAt(0);
+			char VR2=VR.charAt(1);
+			int valueVR1=(int) VR1; // (Quelle taille a ce VR1 ? une ou 2 chiffre ? Comment les ajouter 
+			int valueVR2=(int) VR2; // regarder lorsqu'il tournera) AH AH Non gardons le comme ça, il sera transformé en binaire 
+			int ite;
+			for (ite=0; ite<tag.length();ite++) {
+				int elem=tag.charAt(0);
+				list_tempo.add(elem);
+			}
+			
+			list_tempo.add(longueur);
+			
+			for (ite=0; ite<value.length;ite++) {
+			list_tempo.add(value[ite]);
+			}
+			
+			
+		}
+		public void	set_data_VR(int tag, String VR, int longueur, int[] value) {
+			char VR1=VR.charAt(0);
+			char VR2=VR.charAt(1);
+			int valueVR1=(int) VR1;
+			int valueVR2=(int) VR2;
+			//int b=0x4F;
 			
 			
 			
 			
 		}
-		public void	set_data_VR(int nombre, String VR, int longueur, int value) {
+		public void set_valeur(double valeurs[][]) {
+			/*Rentre les valeurs des accélérotmetre dans le dicom 
+			 * 
+			*/
+			
+			
+			
+		}
+		
+		private int[] translate(){
+			/*Transforme une ArrayList en int[]*/
+			int ite;
+			int MaxL=list_tempo.size();
+			int[] list_tempo_int=new int[MaxL];
+			for (ite =0; ite<MaxL;ite++) {
+				list_tempo_int[ite] =list_tempo.get(ite);
+				}
+				
+			}
+			
 		
 			
 			
-			
-			
 		}
-		/*public void open (String NomDuFichier) {
-			Salut des soucisses
-		}*/
+		public ByteArrayOutputStream export() {
+			byte[] table_tempo=toArray(list_tempo);
+			ByteArrayOutputStream Sortie = new ByteArrayOutputStream(table_tempo.length);
+			Sortie.write(table_tempo);
+			return Sortie;
+		}
 		
 
 }
--- projet_dicom_writer/src/projet_dicom_writer/Main_write.java
+++ projet_dicom_writer/src/projet_dicom_writer/Main_write.java
@@ -3,11 +3,13 @@ package projet_dicom_writer;
 public class Main_write {
 
 	public static void main(String[] args) {
-		// vincentecouteTODO Auto-generated method stub
-		// Heloo ça va ? 
-
+		//TODO Auto-generated method stub 
+		double Data_accel[][] ; //fonction qui récupère le csv // = new Dicom_bluetooth();
+		// ne pas oublier les try catch 
 		Dicom_write myFichier = new Dicom_write();
 		myFichier.set_nom_fichier(args[0]);	
+		myFichier.set_valeur(Data_accel);
+		
 
 
 	}
