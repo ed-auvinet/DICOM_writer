70e3e6ba9ee6d09d4893d7bf2b64ea8b1d18c319
antoninhalut <74411863+antoninhalut@users.noreply.github.com>
2020-11-27 20:47:43 +0100

    Dicom_write_version 2 update closer than ever

--- projet_dicom_writer/src/projet_dicom_writer/Dicom_write.java
+++ projet_dicom_writer/src/projet_dicom_writer/Dicom_write.java
@@ -1,7 +1,7 @@
 package projet_dicom_writer;
 import java.io.*;
-import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
+
 
 
 
@@ -9,22 +9,55 @@ public class Dicom_write {
 	private String nom_fichier;
 	private ByteArrayOutputStream Sortie;
 	private byte octetvalue;
-	private ArrayList<Integer> list_tempo; //utiliser list<Integer> instead ?
-	
+	private ArrayList<Byte> list_tempo; //utiliser list<Integer> instead ?
+	//utiliser le ArrayList<byte> à la place pour 
 	//package dicom_writer;
-	public Dicom_write(int total_size) {
+	
+	public Dicom_write() {
 		/*Prepare le dicom avec le preambule et la norme*/
 		int i;
-		for (i=0; i<127;i++) { 
-				list_tempo.add(0x00);	//plus sûr on aurait pu utilisé 0 aussi peut-être
+		for (i=0; i==4;i++) { 
+				byte elem=(int) 0; // ajout 32 octet de 0 4x =128 octets 
+				list_tempo.add(elem);
 		}
-		list_tempo.add(0x44);
-		list_tempo.add(0x49);
-		list_tempo.add(0x43);
-		list_tempo.add(0x4D);
+		list_tempo.add((byte) 'D'); //0x44);  quelle taille fait 0x..
+		list_tempo.add((byte) 'I'); //0x49);
+		list_tempo.add((byte) 'C'); //0x43);
+		list_tempo.add((byte) 'M'); //0x4D);
+		
 	}
 	
-	
+	public void addTag(char tag) {
+		//en endian, à ajouter 
+		list_tempo.add((byte) tag);
+		
+	}
+	public void addVRstrict(char VR) {
+		//en endian, à ajouter 
+		list_tempo.add((byte) VR);
+		
+	}
+	public void addVR(char VR) {
+		//en endian, à ajouter 
+		list_tempo.add((byte) VR);
+		
+	}
+	public void addValueLengStrict(int longueur) {
+		//int unsigned remplacé par int tant qu'on dépasse pas les 2 millions
+		//en endian, à ajouter 
+		list_tempo.add((byte) longueur);
+		
+	}
+	public void addValueLeng(char longueur) {
+		//en endian, à ajouter 
+		list_tempo.add((byte) longueur);
+		
+	}
+	public void addValue(int valeur) {
+		//en endian, à ajouter 
+		list_tempo.add((byte) valeur);
+		
+	}
 	
 	public void set_nom_fichier(String nom) {
 			this.nom_fichier=nom;
@@ -35,64 +68,24 @@ public class Dicom_write {
 			 * Utilisation du code Endian Data Input Stream modifié.... à ajouter ici à chaque étapes */
 			char VR1=VR.charAt(0);
 			char VR2=VR.charAt(1);
-			int valueVR1=(int) VR1; // (Quelle taille a ce VR1 ? une ou 2 chiffre ? Comment les ajouter 
-			int valueVR2=(int) VR2; // regarder lorsqu'il tournera) AH AH Non gardons le comme ça, il sera transformé en binaire 
+			// (Quelle taille a ce VR1 ? une ou 2 chiffre ? Comment les ajouter 
+			// regarder lorsqu'il tournera) AH AH Non gardons le comme ça, il sera transformé en binaire 
 			int ite;
-			for (ite=0; ite<tag.length();ite++) {
+			for (ite=0; ite<3;ite++) {
 				int elem=tag.charAt(0);
 				list_tempo.add(elem);
 			}
-			
-			list_tempo.add(longueur);
-			
-			for (ite=0; ite<value.length;ite++) {
-			list_tempo.add(value[ite]);
-			}
-			
-			
-		}
-		public void	set_data_VR(int tag, String VR, int longueur, int[] value) {
-			char VR1=VR.charAt(0);
-			char VR2=VR.charAt(1);
-			int valueVR1=(int) VR1;
-			int valueVR2=(int) VR2;
-			//int b=0x4F;
-			
-			
-			
-			
-		}
-		public void set_valeur(double valeurs[][]) {
-			/*Rentre les valeurs des accélérotmetre dans le dicom 
-			 * 
-			*/
-			
-			
-			
+
 		}
 		
-		private byte[] translate(){
-			/*Transforme une ArrayList en int[]
-			 * MODIFICATION transforme ArrayList<Integer> to byte[] !*/
-			int ite;
-			int MaxL=list_tempo.size();
-			byte[] list_tempo_byte=new byte[MaxL];
-			for (ite =0; ite<MaxL;ite++) {
-				int temp=list_tempo.get(ite); // Si  problème, remplacer en Integer
-				list_tempo_byte[ite] =(byte) temp; //byteValue si Integer
-				}
-			return list_tempo_byte;
-				
-			}
-			
 		
-			
-			
-		}
-		public ByteArrayOutputStream export() {
-			byte[] list_tempo_byte=translate();
-			ByteArrayOutputStream Sortie = new ByteArrayOutputStream(table_tempo.length);
-			Sortie.write(list_tempo_byte);
+		public ByteArrayOutputStream export() throws IOException {
+			byte[] result = new byte[list_tempo.size()];
+			for(int i = 0; i < list_tempo.size(); i++) {
+			    result[i] = list_tempo.get(i);// .byteValue(); ajoute ça si marche pas ? 
+			}
+			ByteArrayOutputStream Sortie = new ByteArrayOutputStream(result.length); 
+			Sortie.write(result);
 			return Sortie;
 		}
 		
--- projet_dicom_writer/src/projet_dicom_writer/Main_write.java
+++ projet_dicom_writer/src/projet_dicom_writer/Main_write.java
@@ -4,9 +4,17 @@ public class Main_write {
 
 	public static void main(String[] args) {
 		//TODO Auto-generated method stub 
-		double Data_accel[][] ; //fonction qui récupère le csv // = new Dicom_bluetooth();
+		double Data_accel[] ; //fonction qui récupère le csv // = new Dicom_bluetooth();
+		
+		//Data_user myData= new Data_user(); objet qui contient toutes les sauvegardes de l'application
+		//myData.getTag;
+		//myData.getVR;
+		//MyData.getValue;
 		// ne pas oublier les try catch 
 		Dicom_write myFichier = new Dicom_write();
+		
+		// Ajouter les ajout de valeur, attention doivent être dans l'ordre, fonction qui doit les trier ! 
+		
 		myFichier.set_nom_fichier(args[0]);	
 		myFichier.set_valeur(Data_accel);
 		
